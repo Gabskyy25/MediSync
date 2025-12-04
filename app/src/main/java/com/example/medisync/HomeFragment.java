9faaaef34be720b1803eca2341604df3689bb29b
Gabskyy25 <jangabrieldeguzman25@gmail.com>
2025-12-04 21:21:35 +0800

    initial commit

--- app/src/main/java/com/example/medisync/HomeFragment.java
+++ app/src/main/java/com/example/medisync/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.medisync;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,8 +11,19 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView slider;
+    private Handler handler = new Handler();
+    private int position = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,19 +39,41 @@ public class HomeFragment extends Fragment {
         ImageView contactBtn = view.findViewById(R.id.contact);
         ImageView scheduleBtn = view.findViewById(R.id.schedule);
 
-        alarmBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), alarm.class);
-            startActivity(intent);
-        });
+        alarmBtn.setOnClickListener(v -> startActivity(new Intent(getActivity(), alarm.class)));
+        contactBtn.setOnClickListener(v -> startActivity(new Intent(getActivity(), contactinfo.class)));
+        scheduleBtn.setOnClickListener(v -> startActivity(new Intent(getActivity(), schedule.class)));
 
-        contactBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), contactinfo.class);
-            startActivity(intent);
-        });
+        slider = view.findViewById(R.id.imageSlider);
 
-        scheduleBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(getActivity(), schedule.class);
-            startActivity(intent);
-        });
+        List<Integer> images = Arrays.asList(
+                R.drawable.image1,
+                R.drawable.image2,
+                R.drawable.image3,
+                R.drawable.image4,
+                R.drawable.image5,
+                R.drawable.image6
+        );
+
+        ImageSliderAdapter adapter = new ImageSliderAdapter(getContext(), images);
+        slider.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager =
+                new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        slider.setLayoutManager(layoutManager);
+
+        SnapHelper snapHelper = new PagerSnapHelper();
+        snapHelper.attachToRecyclerView(slider);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (position == images.size()) {
+                    position = 0;
+                }
+                slider.smoothScrollToPosition(position);
+                position++;
+                handler.postDelayed(this, 3000);
+            }
+        }, 3000);
     }
 }
